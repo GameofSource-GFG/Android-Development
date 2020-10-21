2e7f4b78907937be9a687681ced73ec3a8991d3e
Rithik Bhandari <rithikbhandari895@gmail.com>
2020-10-21 13:07:47 +0530

    changed folder name from views to screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:our_gfg/views/upcoming_events_screen.dart';
+import 'package:our_gfg/screens/upcoming_events_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
