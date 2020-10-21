7e0060ecf66eaa69f089a40c38fc4b29220869cf
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-10-21 22:34:08 +0530

    added_constraints

---
+++ lib/models/constraints.dart
@@ -0,0 +1,27 @@
+class MEMBER {
+  String speakerName;
+  String speakerDesc;
+  String speakerImage;
+  String speakerInfo;
+  String speakerId;
+  String fbUrl;
+  String twitterUrl;
+  String linkedinUrl;
+  String githubUrl;
+  String speakerSession;
+  String instagramUrl;
+
+  MEMBER({
+    this.speakerName,
+    this.speakerDesc,
+    this.speakerImage,
+    this.speakerInfo,
+    this.speakerId,
+    this.fbUrl,
+    this.twitterUrl,
+    this.linkedinUrl,
+    this.githubUrl,
+    this.speakerSession,
+    this.instagramUrl,
+  });
+}
