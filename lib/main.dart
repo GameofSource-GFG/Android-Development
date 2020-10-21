99cf4c8bd1d9019ecb0bf4a0ad3c4a386b882e5e
Varun S Athreya <varun.sathreya@gmail.com>
2020-10-21 14:08:47 +0530

    fixed merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:our_gfg/screens/social_media.dart';
 import 'package:our_gfg/screens/upcoming_events_screen.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,9 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
         appBarTheme: AppBarTheme(color: kMainColor),
         accentColor: kMainColor,
         visualDensity: VisualDensity.adaptivePlatformDensity,
