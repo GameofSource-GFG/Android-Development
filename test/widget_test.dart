9a71c9432c6bb74aa3446a82929801203b1eddd7
Harsh Gupta <gharshgupta29@gmail.com>
2020-10-22 02:00:11 +0530

    Added mission/vission page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:our_gfg/main.dart';
+import 'package:our_gfg/mission.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
