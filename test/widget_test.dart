8df1e2c56b41ea09a772175407763ecd264ea41d
Harsh Gupta <gharshgupta29@gmail.com>
2020-10-22 15:29:21 +0530

    Changes to one page.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:our_gfg/mission.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(AfterSplash());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
