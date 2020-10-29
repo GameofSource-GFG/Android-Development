d6e90a336f41f5c1b2d62e766238456279740794
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-10-30 01:08:59 +0530

    updated

--- lib/components/upcoming_event_card.dart
+++ lib/components/upcoming_event_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../screens/EventRegistration.dart';
 import 'my_cached_network_image.dart';
 import '../models/event.dart';
 
@@ -84,6 +85,14 @@ class UpcomingEventCard extends StatelessWidget {
               ],
             ),
           ),
+          RaisedButton(
+              child: Text('Register Now'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => EventRegistration()),
+                );
+              })
         ],
       ),
     );
