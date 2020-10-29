72fdd74c1dbf22476dff67c0891900e0d6a3d27b
kanhayaKy <kaku18is@cmrit.ac.in>
2020-10-29 17:25:43 +0530

    Added Event Registration

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         ContactUs.routeName: (context) => ContactUs(),
         EventRegistration.routeName: (context) => EventRegistration()
       },
-      home: EventRegistration(),
+      home: LoginScreen(),
     );
   }
 }
