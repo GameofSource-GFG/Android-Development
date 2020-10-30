739a57d6746c8fa4ea17d7ca7577ed16b5c6f216
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2020-10-30 21:54:15 +0530

    updated

--- lib/screens/contact_us_screen.dart
+++ lib/screens/contact_us_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:our_gfg/components/embedded_map.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 import '../services/firebase_storage_service.dart';
@@ -99,16 +100,7 @@ class ContactUs extends StatelessWidget {
               ),
               Column(
                 children: [
-                  Container(
-                    height: 215,
-                    child: WebView(
-                      initialUrl: Uri.dataFromString(
-                        '<html><body><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d62206.04005409677!2d77.61683905!3d12.97968585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1603287999342!5m2!1sen!2sin" allowfullscreen="" width="${MediaQuery.of(context).size.width - 15}" height="200" frameborder="0" style="border:0;" aria-hidden="false" tabindex="0"></iframe></body></html>',
-                        mimeType: 'text/html',
-                      ).toString(),
-                      javascriptMode: JavascriptMode.unrestricted,
-                    ),
-                  )
+                  EmbeddedMap(),
                 ],
               )
             ],
