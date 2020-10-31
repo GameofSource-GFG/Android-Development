f7328104cd8aea931c9150cda2ed3dc5c7eaaaff
Varun S Athreya <varun.sathreya@gmail.com>
2020-10-31 16:48:52 +0530

    reoved unused import

--- lib/widgets/CustomAppDrawer.dart
+++ lib/widgets/CustomAppDrawer.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:share/share.dart';
 
 import '../screens/EventRegistration.dart';
